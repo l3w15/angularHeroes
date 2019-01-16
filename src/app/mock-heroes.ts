317023461da615f0926555a9a4af5f43449c2f45
Lewis Watson <lewis.watson@sainsburys.co.uk>
2019-01-16 11:21:49 +0000

    Add hero list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Firefly'},
+    { id: 12, name: 'Wolverine'},
+    { id: 13, name: 'Black Panther'},
+    { id: 14, name: 'Professor X'},
+    { id: 15, name: 'Jean Valjean'},
+    { id: 16, name: 'Mantos'},
+    { id: 17, name: 'Black Widow'},
+    { id: 18, name: 'Storm'},
+    { id: 19, name: 'Dark Pheonix'},
+    { id: 20, name: 'Eleanor'}
+]
